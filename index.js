983fbd931e68911740650985b54db4e9d9e6c8e1
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-01-11 12:03:10 +0600

    4rd task Complete.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygp3m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
