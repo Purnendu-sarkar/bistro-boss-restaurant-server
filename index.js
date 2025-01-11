01201e899af72d86cb760f323dff6980e558482d
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-01-11 18:40:56 +0600

    added user delete api

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
